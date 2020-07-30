156f634e8b6728b2eb61b303181502ae5206af91
ayushraj17 <meayushraj17@gmail.com>
2020-07-30 23:12:39 +0530

    fixed data not showing

--- src/App.js
+++ src/App.js
@@ -1,50 +1,44 @@
-import React ,{Component} from 'react';
-import './App.css';
-import axios from 'axios';
-import Form from './Components/Form';
-
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
+import Form from "./Components/Form";
 
 var config = {
-  headers:{
-    "x-rapidapi-host": "the-cocktail-db.p.rapidapi.com",
-	  "x-rapidapi-key": "768eca3f26msh7690703b4fd8819p1880dbjsnf044f41e03ad"
-	
-  }
-}
-
+	headers: {
+		"x-rapidapi-host": "the-cocktail-db.p.rapidapi.com",
+		"x-rapidapi-key": "768eca3f26msh7690703b4fd8819p1880dbjsnf044f41e03ad",
+	},
+};
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      ingredients: []
-    }
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			ingredients: [],
+		};
+	}
 
-componentDidMount() {
-    axios.get('https://the-cocktail-db.p.rapidapi.com/list.php?i=list',config)
-    .then(ingredients => {
-      this.setState({ 
-      ingredients: ingredients 
-    })
-    })
-    .catch(e=>{
-      console.log(e)
-      this.setState({...this.state,isFetching:false});
-    });
-  };
-  
-  
-  render() {
-    return (
-      <div>
-        <Form ingredients={this.state.ingredients}/>
-      </div>
-    )
-  }
+	componentDidMount() {
+		axios
+			.get("https://the-cocktail-db.p.rapidapi.com/list.php?i=list", config)
+			.then((ingredients) => {
+				this.setState({
+					ingredients: ingredients.data.drinks,
+				});
+			})
+			.catch((e) => {
+				console.log(e);
+				this.setState({ ...this.state, isFetching: false });
+			});
+	}
+
+	render() {
+		return (
+			<div>
+				<Form ingredients={this.state.ingredients} />
+			</div>
+		);
+	}
 }
 
 export default App;
-
-
-
--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,27 +1,44 @@
-import React from 'react';
+// import React from "react";
 
-const Form = ( ingredients ) => {
-    return (
-    <div className="data">
-      <table>
-        <thead>
-          <tr>
-            <th>Id</th>
-            <th>Name</th>
-          </tr>
-        </thead>
-        <tbody>
-          { (ingredients.length > 0) ? ingredients.map( (ingredients, index) => {
-             return (
-              <tr key={ index }>
-                <td>{ ingredients.id }</td>
-                <td>{ ingredients.name }</td>
-              </tr>
-            )
-           }) : <tr><td colSpan="5">Loading...</td></tr> }
-        </tbody>
-      </table>
-      </div>
-    );
-  }
-  export default Form
+// const Form = (ingredients) => {
+// 	return (
+// 		<div className="data">
+// 			<table>
+// 				<thead>
+// 					<tr>
+// 						<th>Id</th>
+// 						<th>Name</th>
+// 					</tr>
+// 				</thead>
+// 				<tbody>
+// 					{ingredients.length > 0 ? (
+// 						ingredients.map(({ strIngredient1, i }) => {
+// 							return (
+// 								<tr key={i}>
+// 									<td>{strIngredient1}</td>
+// 									<td>{ingredients.name}</td>
+// 								</tr>
+// 							);
+// 						})
+// 					) : (
+// 						<tr>
+// 							<td colSpan="5">Loading...</td>
+// 						</tr>
+// 					)}
+// 				</tbody>
+// 			</table>
+// 		</div>
+// 	);
+// };
+// export default Form;
+
+import React from "react";
+
+function Form({ ingredients }) {
+	// console.log(ingredients);
+	return ingredients.map(({ strIngredient1, i }) => (
+		<div key={i}>{strIngredient1}</div>
+	));
+}
+
+export default Form;
